e0746a632d2d6c0a8587cf1988c3dff5d63bc029
Dmitry Malyshev <damalyshev@gmail.com>
2022-07-06 09:37:23 +0300

    .

--- part04-e02_powers_of_series/src/powers_of_series.py
+++ part04-e02_powers_of_series/src/powers_of_series.py
@@ -2,11 +2,18 @@
 
 import pandas as pd
 
-def powers_of_series(s, k):
-    return None
-    
+
+def powers_of_series(s: pd.Series, k: int) -> pd.DataFrame:
+    df = pd.DataFrame(s, columns=[1])
+    for i in range(2, k + 1):
+        df[i] = s**i
+    return df
+
+
 def main():
-    return
-    
+    s = pd.Series([1, 2, 3, 4], index=list("abcd"))
+    print(powers_of_series(s, 3))
+
+
 if __name__ == "__main__":
     main()
