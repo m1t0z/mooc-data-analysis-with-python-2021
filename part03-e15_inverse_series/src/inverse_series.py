5938c423e82cf40d81f38242fd4c3cbc4a7e8201
Dmitry Malyshev <damalyshev@gmail.com>
2022-07-06 09:05:01 +0300

    .

--- part03-e15_inverse_series/src/inverse_series.py
+++ part03-e15_inverse_series/src/inverse_series.py
@@ -2,11 +2,18 @@
 
 import pandas as pd
 
-def inverse_series(s):
-    return pd.Series()
+
+def inverse_series(s: pd.Series):
+    return pd.Series(s.index, s.values)
+
 
 def main():
-    return
+    s = pd.Series([0, 1], list("ab"), name="s")
+    s_inversed = inverse_series(s)
+    s_inversed.name = "s_inversed"
+
+    print(s, s_inversed, sep="\n")
+
 
 if __name__ == "__main__":
     main()
