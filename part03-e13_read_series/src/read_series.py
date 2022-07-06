35ca8e815518bf65384f97b2d34bebfc5cea9a79
Dmitry Malyshev <damalyshev@gmail.com>
2022-07-06 08:48:07 +0300

    .

--- part03-e13_read_series/src/read_series.py
+++ part03-e13_read_series/src/read_series.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python3
+from ctypes import ArgumentError
+import pandas as pd
 
 
 def read_series():
-    return pd.Series()
+    data: list[str] = []
+    index: list[str] = []
+    while line := input():
+        words = line.split()
+        if len(words) != 2:
+            raise Exception(f"2 words are expected but {len(words)} are given!")
+
+        index.append(words[0])
+        data.append(words[1])
+
+    return pd.Series(data, index)
+
 
 def main():
-    pass
+    print(read_series())
+
 
 if __name__ == "__main__":
     main()
