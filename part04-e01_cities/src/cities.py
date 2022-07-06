bfde84b0a6c2c72881047486eaa03b24361b7529
Dmitry Malyshev <damalyshev@gmail.com>
2022-07-06 09:28:22 +0300

    .

--- part04-e01_cities/src/cities.py
+++ part04-e01_cities/src/cities.py
@@ -2,11 +2,17 @@
 
 import pandas as pd
 
-def cities():
-    return None
-    
+
+def cities() -> pd.DataFrame:
+    index = ["Helsinki", "Espoo", "Tampere", "Vantaa", "Oulu"]
+    s_population = pd.Series([643272, 279044, 231853, 223027, 201810], index)
+    s_area = pd.Series([715.48, 528.03, 689.59, 240.35, 3817.52], index)
+    return pd.DataFrame({"Population": s_population, "Total area": s_area})
+
+
 def main():
-    return
-    
+    print(cities())
+
+
 if __name__ == "__main__":
     main()
