faed85040033e6cff423143751b4aa8286931f1f
Dmitry Malyshev <damalyshev@gmail.com>
2022-07-07 09:29:15 +0300

    .

--- part04-e05_swedish_and_foreigners/src/swedish_and_foreigners.py
+++ part04-e05_swedish_and_foreigners/src/swedish_and_foreigners.py
@@ -2,11 +2,34 @@
 
 import pandas as pd
 
+
 def swedish_and_foreigners():
-    return None
+    df = pd.read_csv("src/municipal.tsv", sep="\t", index_col=0)
+
+    # Subset of rows: only municipalities.
+    df = df["Akaa":"Äänekoski"]
+
+    # Subset of rows: proportion of Swedish speaking people and proportion of foreigners both above 5 % level.
+    df = df[
+        (df["Share of Swedish-speakers of the population, %"] > 5)
+        & (df["Share of foreign citizens of the population, %"] > 5)
+    ]
+
+    # Take only 3 columns.
+    df = df[
+        [
+            "Population",
+            "Share of Swedish-speakers of the population, %",
+            "Share of foreign citizens of the population, %",
+        ]
+    ]
+
+    return df
+
 
 def main():
-    return
+    print(swedish_and_foreigners())
+
 
 if __name__ == "__main__":
     main()
