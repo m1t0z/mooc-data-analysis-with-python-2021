1888b541e3de6104fcbace1733ccf18fb5584e01
Dmitry Malyshev <damalyshev@gmail.com>
2022-07-07 09:58:36 +0300

    .

--- part04-e06_growing_municipalities/src/growing_municipalities.py
+++ part04-e06_growing_municipalities/src/growing_municipalities.py
@@ -2,11 +2,22 @@
 
 import pandas as pd
 
-def growing_municipalities(df):
-    0.0
+
+def growing_municipalities(df: pd.DataFrame):
+
+    # Subset: municipalities with increasing population.
+    df_inc_population = df[df["Population change from the previous year, %"] > 0]
+
+    ratio = df_inc_population.shape[0] / df.shape[0]
+    return ratio
+
 
 def main():
-    return
+    df = pd.read_csv("src/municipal.tsv", sep="\t", index_col=0)
+    df = df["Akaa":"Äänekoski"]
+    percent = growing_municipalities(df) * 100
+    print(f"Proportion of growing municipalities: {percent:.1f}%")
+
 
 if __name__ == "__main__":
     main()
