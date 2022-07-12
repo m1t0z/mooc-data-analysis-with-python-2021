2db02f613a4997fb1139057deb6b11f2a94508f6
Dmitry Malyshev <damalyshev@gmail.com>
2022-07-12 09:12:36 +0300

    .

--- part04-e09_snow_depth/src/snow_depth.py
+++ part04-e09_snow_depth/src/snow_depth.py
@@ -2,11 +2,15 @@
 
 import pandas as pd
 
-def snow_depth():
-    return 0.0
+
+def snow_depth() -> float:
+    df = pd.read_csv("src/kumpula-weather-2017.csv")
+    return df["Snow depth (cm)"].max()
+
 
 def main():
-    return
+    print(f"Max snow depth: {snow_depth():2.1f}")
+
 
 if __name__ == "__main__":
     main()
