475627238084a12508d3d106ea95da4435b293e1
Dmitry Malyshev <damalyshev@gmail.com>
2022-07-12 08:55:10 +0300

    .

--- part04-e07_subsetting_with_loc/src/subsetting_with_loc.py
+++ part04-e07_subsetting_with_loc/src/subsetting_with_loc.py
@@ -2,11 +2,23 @@
 
 import pandas as pd
 
-def subsetting_with_loc():
-    return None
+
+def subsetting_with_loc() -> pd.DataFrame:
+    df = pd.read_csv("src/municipal.tsv", sep="\t", index_col=0)
+    subset = df.loc[
+        "Akaa":"Äänekoski",
+        [
+            "Population",
+            "Share of Swedish-speakers of the population, %",
+            "Share of foreign citizens of the population, %",
+        ],
+    ]
+    return subset
+
 
 def main():
-    return
+    print(subsetting_with_loc())
+
 
 if __name__ == "__main__":
     main()
