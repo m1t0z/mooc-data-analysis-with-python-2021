51fe9d85a0afc2995c0f19efc285552b7e72ee7e
Dmitry Malyshev <damalyshev@gmail.com>
2022-07-12 09:33:50 +0300

    .

--- part04-e12_cyclists/src/cyclists.py
+++ part04-e12_cyclists/src/cyclists.py
@@ -2,12 +2,17 @@
 
 import pandas as pd
 
-def cyclists():
-    return None
+
+def cyclists() -> pd.DataFrame:
+    df = pd.read_csv("src/Helsingin_pyorailijamaarat.csv", sep=";")
+    df_clean = df.dropna(axis=0, how="all").dropna(axis=1, how="all")
+    return df_clean
 
 
 def main():
-    return
-    
+    df = cyclists()
+    print(df)
+
+
 if __name__ == "__main__":
     main()
