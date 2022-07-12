4dd87e824cceb738e456eb3837132eb783cdda6a
Dmitry Malyshev <damalyshev@gmail.com>
2022-07-12 09:20:46 +0300

    .

--- part04-e11_below_zero/src/below_zero.py
+++ part04-e11_below_zero/src/below_zero.py
@@ -2,11 +2,15 @@
 
 import pandas as pd
 
-def below_zero():
-    return 0
+
+def below_zero() -> int:
+    df = pd.read_csv("src/kumpula-weather-2017.csv")
+    return sum(df["Air temperature (degC)"] < 0)
+
 
 def main():
-    return
-    
+    print(f"Number of days below zero: {below_zero()}")
+
+
 if __name__ == "__main__":
     main()
