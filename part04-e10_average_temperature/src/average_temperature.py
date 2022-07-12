df1f72c0f2d7c16a6e5b64fb5d1ca57059b85c0c
Dmitry Malyshev <damalyshev@gmail.com>
2022-07-12 09:16:43 +0300

    .

--- part04-e10_average_temperature/src/average_temperature.py
+++ part04-e10_average_temperature/src/average_temperature.py
@@ -2,11 +2,16 @@
 
 import pandas as pd
 
-def average_temperature():
-    return 0.0
+
+def average_temperature() -> float:
+    df = pd.read_csv("src/kumpula-weather-2017.csv")
+    df_july = df[df["m"] == 7]
+    return df_july["Air temperature (degC)"].mean()
+
 
 def main():
-    return
+    print(f"Average temperature in July: {average_temperature():.1f}")
+
 
 if __name__ == "__main__":
     main()
