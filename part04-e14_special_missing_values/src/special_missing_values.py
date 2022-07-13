d3eb755a4abd797a29558f8b6da50d3cb2dada01
Dmitry Malyshev <damalyshev@gmail.com>
2022-07-13 09:56:07 +0300

    .

--- part04-e14_special_missing_values/src/special_missing_values.py
+++ part04-e14_special_missing_values/src/special_missing_values.py
@@ -3,11 +3,26 @@
 import pandas as pd
 import numpy as np
 
-def special_missing_values():
-    return None
+
+def special_missing_values() -> pd.DataFrame:
+    df = pd.read_csv("src/UK-top40-1964-1-2.tsv", sep="\t")
+
+    # Cast "LW" column from str to float ("Re", "New" values are replaced with NaN).
+    df["LW"] = pd.to_numeric(df["LW"], errors="coerce")
+
+    # Keep only the records that drop their position.
+    m = df["Pos"] > df["LW"]
+    df = df[m]
+
+    # Cast "LW" columnt from float to int.
+    df["LW"] = pd.to_numeric(df["LW"], downcast="integer")
+
+    return df
+
 
 def main():
-    return
+    print(special_missing_values())
+
 
 if __name__ == "__main__":
     main()
