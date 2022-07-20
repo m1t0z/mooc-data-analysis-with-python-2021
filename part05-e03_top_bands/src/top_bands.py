4309671e2a9482b8af5b21097d736c93d51375df
Dmitry Malyshev <damalyshev@gmail.com>
2022-07-20 09:09:36 +0300

    .

--- part05-e03_top_bands/src/top_bands.py
+++ part05-e03_top_bands/src/top_bands.py
@@ -2,11 +2,21 @@
 
 import pandas as pd
 
-def top_bands():
-    return pd.DataFrame()
+
+def top_bands() -> pd.DataFrame:
+    df_top = pd.read_csv("src/UK-top40-1964-1-2.tsv", sep="\t")
+    df_bands = pd.read_csv("src/bands.tsv", sep="\t")
+
+    df_top["Artist"] = df_top["Artist"].str.upper()
+    df_bands["Band"] = df_bands["Band"].str.upper()
+
+    df = pd.merge(df_top, df_bands, left_on="Artist", right_on="Band")
+    return df
+
 
 def main():
-    return
+    print(top_bands())
+
 
 if __name__ == "__main__":
     main()
