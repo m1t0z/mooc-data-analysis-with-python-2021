fd68b81d0319348ae18a61b28660fb0bb2aa3284
Dmitry Malyshev <damalyshev@gmail.com>
2022-07-21 08:51:25 +0300

    .

--- part05-e05_best_record_company/src/best_record_company.py
+++ part05-e05_best_record_company/src/best_record_company.py
@@ -2,12 +2,17 @@
 
 import pandas as pd
 
-def best_record_company():
-    return None
+
+def best_record_company() -> pd.DataFrame():
+    df = pd.read_csv("src/UK-top40-1964-1-2.tsv", sep="\t")
+    df_grouped_by_publisher = df.groupby("Publisher")
+    best_publisher_idx = df_grouped_by_publisher["WoC"].sum().idxmax()
+    return df_grouped_by_publisher.get_group(best_publisher_idx)
+
 
 def main():
-    return
-    
+    print(best_record_company())
+
 
 if __name__ == "__main__":
     main()
