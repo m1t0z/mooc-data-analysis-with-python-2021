bc292cb289ca4d50478fcc4bba0659ca17897ef9
Dmitry Malyshev <damalyshev@gmail.com>
2022-07-21 09:28:40 +0300

    .

--- part05-e06_suicide_fractions/src/suicide_fractions.py
+++ part05-e06_suicide_fractions/src/suicide_fractions.py
@@ -2,11 +2,16 @@
 
 import pandas as pd
 
-def suicide_fractions():
-    return pd.Series()
+
+def suicide_fractions() -> pd.Series:
+    df = pd.read_csv("src/who_suicide_statistics.csv")
+    df["fraction"] = df["suicides_no"] / df["population"]
+    return df.groupby("country")["fraction"].mean()
+
 
 def main():
-    return
+    print(suicide_fractions())
+
 
 if __name__ == "__main__":
     main()
