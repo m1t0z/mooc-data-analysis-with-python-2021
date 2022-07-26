f1422faecaae2f504a0d25d85b3216ec1dc65902
Dmitry Malyshev <damalyshev@gmail.com>
2022-07-26 08:45:41 +0300

    .

--- part05-e11_mystery_data/src/mystery_data.py
+++ part05-e11_mystery_data/src/mystery_data.py
@@ -3,12 +3,26 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 
+
 def mystery_data():
-    return []
+    df = pd.read_csv("src/mystery_data.tsv", sep="\t")
+
+    y = df["Y"]
+    X = df.iloc[:, :-1]
+
+    # print(X.shape)
+    # print(y.shape)
+
+    reg = LinearRegression(fit_intercept=False).fit(X, y)
+
+    return reg.coef_
+
 
 def main():
     coefficients = mystery_data()
-    # print the coefficients here
-    
+    for i, c in enumerate(coefficients):
+        print(f"Coefficient of X{i+1} is {c}")
+
+
 if __name__ == "__main__":
     main()
