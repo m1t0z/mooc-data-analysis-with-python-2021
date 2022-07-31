a2a7d12192b58267e37cbc370c49a50f59091f87
Dmitry Malyshev <damalyshev@gmail.com>
2022-07-31 08:39:54 +0300

    .

--- part06-e02_plant_classification/src/plant_classification.py
+++ part06-e02_plant_classification/src/plant_classification.py
@@ -5,11 +5,22 @@ from sklearn.model_selection import train_test_split
 from sklearn import naive_bayes
 from sklearn import metrics
 
+
 def plant_classification():
-    return 0.0
+    X, y = load_iris(return_X_y=True)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, train_size=0.8, random_state=0
+    )
+
+    model = naive_bayes.GaussianNB()
+    model.fit(X_train, y_train)
+    y_predicted = model.predict(X_test)
+    return metrics.accuracy_score(y_test, y_predicted)
+
 
 def main():
     print(f"Accuracy is {plant_classification()}")
+
 
 if __name__ == "__main__":
     main()
