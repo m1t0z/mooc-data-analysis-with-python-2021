407f8256d6f8f897b4a5aa6863caf6101598bac9
Dmitry Malyshev <damalyshev@gmail.com>
2022-07-31 08:34:58 +0300

    .

--- part06-e01_blob_classification/src/blob_classification.py
+++ part06-e01_blob_classification/src/blob_classification.py
@@ -6,23 +6,35 @@ from sklearn import metrics
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 
+
 def blob_classification(X, y):
-    return 0.0
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, train_size=0.75, random_state=0
+    )
+    model = GaussianNB()
+    model.fit(X_train, y_train)
+    y_predicted = model.predict(X_test)
+    return metrics.accuracy_score(y_test, y_predicted)
+
 
 def main():
-    X,y = datasets.make_blobs(100, 2, centers=2, random_state=2, cluster_std=2.5)
+    X, y = datasets.make_blobs(100, 2, centers=2, random_state=2, cluster_std=2.5)
     print("The accuracy score is", blob_classification(X, y))
-    a=np.array([[2, 2, 0, 2.5],
-                [2, 3, 1, 1.5],
-                [2, 2, 6, 3.5],
-                [2, 2, 3, 1.2],
-                [2, 4, 4, 2.7]])
-    accs=[]
+    a = np.array(
+        [[2, 2, 0, 2.5], [2, 3, 1, 1.5], [2, 2, 6, 3.5], [2, 2, 3, 1.2], [2, 4, 4, 2.7]]
+    )
+    accs = []
     for row in a:
-        X,y = datasets.make_blobs(100, int(row[0]), centers=int(row[1]),
-                                  random_state=int(row[2]), cluster_std=row[3])
+        X, y = datasets.make_blobs(
+            100,
+            int(row[0]),
+            centers=int(row[1]),
+            random_state=int(row[2]),
+            cluster_std=row[3],
+        )
         accs.append(blob_classification(X, y))
-    print(repr(np.hstack([a, np.array(accs)[:,np.newaxis]])))
+    print(repr(np.hstack([a, np.array(accs)[:, np.newaxis]])))
+
 
 if __name__ == "__main__":
     main()
