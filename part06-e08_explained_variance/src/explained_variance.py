4a8b7460b67e16c8e66ffc966291485cf8463e08
Dmitry Malyshev <damalyshev@gmail.com>
2022-08-05 09:13:26 +0300

    .

--- part06-e08_explained_variance/src/explained_variance.py
+++ part06-e08_explained_variance/src/explained_variance.py
@@ -1,12 +1,34 @@
 #!/usr/bin/env python3
+import pandas as pd
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def explained_variance():
-    return [], []
+    df = pd.read_csv("src/data.tsv", sep="\t")
+    pca = PCA()
+    pca.fit(df)
+    ev = pca.explained_variance_
+    v = df.var()
+
+    return v, ev
+
+
+def stringify(arr):
+    return " ".join(map(lambda x: f"{x:.3f}", arr))
+
 
 def main():
     v, ev = explained_variance()
-    print(sum(v), sum(ev))
+    print("The variances are: " + stringify(v))
+    print("The explained variances after PCA are: " + stringify(ev))
+
+    ev_cumsum = np.cumsum(ev)
+    plt.plot(np.arange(len(ev_cumsum)) + 1, ev_cumsum)
+
+    plt.show()
+
 
 if __name__ == "__main__":
     main()
